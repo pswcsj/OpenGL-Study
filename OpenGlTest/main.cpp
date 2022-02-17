d94a035eff060fdc8a714010cab97e1ccfc09bc6
a <pswxsj@naver.com>
2022-02-17 18:18:48 +0900

    Initial Commit

---
+++ OpenGlTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGlTest
+//
+//  Created by User on 2022/02/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
